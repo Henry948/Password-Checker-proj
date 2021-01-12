51c1e22b9e92c138592f272880eff14d7f1e533c
dahdi <62443126+dahdi@users.noreply.github.com>
2021-01-12 21:38:48 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace PasswordChecker
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      int minLength = 8;
+      string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string lowercase = "abcdefghijklmnopqrstuvwxyz";
+      string digits = "0123456789";
+      string specialChars = "=+-_)(*&^%$£!@#~?/><.,`¬";
+      int score = 0;
+      
+
+      Console.WriteLine("Please Input Password: ");
+      string password = Console.ReadLine();
+
+      if (password.Length >= minLength) {
+        score++;
+      } 
+      if (Tools.Contains(password, uppercase)){
+        score++;
+      }
+      if (Tools.Contains(password, lowercase)) {
+        score++;
+      }
+      if (Tools.Contains(password, digits)) {
+        score++;
+      }
+      if (Tools.Contains(password, specialChars)) {
+        score++;
+      }
+
+      Console.WriteLine(score);
+      
+      switch(score) {
+      case 5:
+      Console.WriteLine("The password is extremely strong");
+      break;
+      case 4:
+      Console.WriteLine("The password is extremely strong");
+      break;
+      case 3:
+      Console.WriteLine("The password is strong");
+      break;
+      case 2:
+      Console.WriteLine("The Password is medium");
+      break;
+      case 1:
+      Console.WriteLine("The password is weak");
+      break;
+      default:
+      Console.WriteLine("The password doesnt meet any of the standards!");
+      break;
+      }
+    }
+  }
+}
